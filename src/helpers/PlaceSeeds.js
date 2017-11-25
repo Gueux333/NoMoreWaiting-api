5baf9b38d22e225bb56cfbd6911c19f77e503498
Gueux333 <32457749+Gueux333@users.noreply.github.com>
2017-11-25 17:09:06 +0100

    placeSeeds

--- src/helpers/PlaceSeeds.js
+++ src/helpers/PlaceSeeds.js
@@ -1,7 +1,7 @@
 export default [
   {
     name: 'Bobar',
-    description: 'Bar',
+    description: 'Super Bar',
     lat: '48.711836',
     lng: '2.209921',
     image: 'http://www.lajauneetlarouge.com/sites/default/files/uploads/Images-RTE/RTEmagicC_657-VieEcole-Bobar.jpg.jpg',
